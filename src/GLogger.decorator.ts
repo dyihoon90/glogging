ae03e6220e43c7ca289a5d0ad8458f819cf75f45
Dingyi <hoon_ding_yi@tech.gov.sg>
2020-10-08 16:07:23 +0800

    [DY]add decorator to log trans in raw function
    - previous decorator only good for decorating class methods
    
    - this adds a decorator function for normal raw function

--- src/GLogger.decorator.ts
+++ src/GLogger.decorator.ts
@@ -2,15 +2,18 @@ import { GLogger, IExpressRequest } from '.';
 import { GLoggerAuditLogger } from './GLogger.auditLogger';
 
 /**
- * Class decorator function that adds logging to all class methods
- * All class methods must take in an IExpressRequest (an extended express.Request) object as first parameter
- * This modifies all class methods except the constructor method, adding logging on successful complete or on error
+ * #### Class decorator function that adds logging to all class methods
+ * - All class methods must take in an IExpressRequest object (extended from express.Request) as first parameter
+ * - Creates success audit log of level `info` on return
+ * - Creates fail audit log of level `warn` on error thrown
+ * - Modifies all class methods except the constructor method
+ * - Arrow functions are _not_ considered class methods
  * @param logger a GLogger instance
  * @param trxModule the transaction module e.g. INBOX_CLAIM
  * @param filename the filename. In Node.js can use __filename (if not webpacked)
  */
-export function LogTransaction(logger: GLogger, trxModule: string, filename?: string) {
-  return function <TResult>(target: Function): void {
+export function LogTransactionsForAllMethods(logger: GLogger, trxModule: string, filename?: string) {
+  return function (target: Function): void {
     for (const propertyName of Object.getOwnPropertyNames(target.prototype)) {
       const descriptor = Object.getOwnPropertyDescriptor(target.prototype, propertyName)!;
       const isMethod = descriptor.value instanceof Function;
@@ -18,32 +21,87 @@ export function LogTransaction(logger: GLogger, trxModule: string, filename?: st
         continue;
       }
 
-      const originalMethod = descriptor.value;
-      descriptor.value = async (req: IExpressRequest, ...args: any[]) => {
-        const startTime = new Date().getTime();
-        try {
-          const result = await originalMethod(req, ...args);
-          const auditLoggerInstance = new GLoggerAuditLogger(logger);
-          auditLoggerInstance.logTransactionSuccess(
-            `Transaction: ${propertyName} success`,
-            { req },
-            { filename, trxName: propertyName, trxModule },
-            startTime
-          );
-          return result as TResult;
-        } catch (e) {
-          const auditLoggerInstance = new GLoggerAuditLogger(logger);
-          auditLoggerInstance.logTransactionFailure(
-            e,
-            { req },
-            { filename, trxName: propertyName, trxModule },
-            startTime
-          );
-          throw e;
-        }
+      const originalMethod: (...args: unknown[]) => unknown = descriptor.value;
+      descriptor.value = (req: IExpressRequest, ...args: unknown[]): any => {
+        return decorateFunctionWithLogs(logger, trxModule, originalMethod, propertyName, req, filename, ...args);
       };
 
       Object.defineProperty(target.prototype, propertyName, descriptor);
     }
   };
+}
+
+/**
+ * #### decorator function that adds logging to a normal function / arrow function
+ * - Function must take in an IExpressRequest object (extended from express.Request) as first parameter
+ * - Creates success audit log of level `info` on return
+ * - Creates fail audit log of level `warn` on error thrown
+ * @param logger a GLogger instance
+ * @param trxModule the transaction module e.g. INBOX_CLAIM
+ * @param filename the filename. In Node.js can use __filename (if not webpacked)
+ */
+export function LogTransaction(logger: GLogger, trxModule: string, filename?: string) {
+  return function <U extends unknown[], V>(
+    decoratedFunc: (req: IExpressRequest, ...args: U) => V,
+    req: IExpressRequest,
+    ...args: U
+  ): Promise<unknown> | V {
+    return decorateFunctionWithLogs(logger, trxModule, decoratedFunc, decoratedFunc.name, req, filename, ...args);
+  };
+}
+
+function decorateFunctionWithLogs<U extends unknown[], V>(
+  logger: GLogger,
+  trxModule: string,
+  decoratedFunc: (req: IExpressRequest, ...args: U) => V,
+  decoratedFuncName: string,
+  req: IExpressRequest,
+  filename?: string,
+  ...args: U
+): Promise<unknown> | V {
+  const startTime = new Date().getTime();
+  try {
+    const auditLoggerInstance = new GLoggerAuditLogger(logger);
+    const promiseOrValue = decoratedFunc(req, ...args);
+    // Scenario where decoratedFunc is asynchronous returning Promise
+    if (promiseOrValue instanceof Promise) {
+      return promiseOrValue
+        .then((result) => {
+          auditLoggerInstance.logTransactionSuccess(
+            `Transaction: ${decoratedFuncName} success`,
+            { req },
+            { filename, trxName: decoratedFuncName, trxModule },
+            startTime
+          );
+          return result as unknown;
+        })
+        .catch((e) => {
+          auditLoggerInstance.logTransactionFailure(
+            e,
+            { req },
+            { filename, trxName: decoratedFuncName, trxModule },
+            startTime
+          );
+          throw e;
+        });
+    }
+    // Scenario where decoratedFunc is synchronous returning value
+    // Why separate? if we `await` sync decoratedFunc, return value gets casted into Promise, becoming async
+    auditLoggerInstance.logTransactionSuccess(
+      `Transaction: ${decoratedFuncName} success`,
+      { req },
+      { filename, trxName: decoratedFuncName, trxModule },
+      startTime
+    );
+    return promiseOrValue;
+  } catch (e) {
+    const auditLoggerInstance = new GLoggerAuditLogger(logger);
+    auditLoggerInstance.logTransactionFailure(
+      e,
+      { req },
+      { filename, trxName: decoratedFuncName, trxModule },
+      startTime
+    );
+    throw e;
+  }
 }
