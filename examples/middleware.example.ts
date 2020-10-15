84392bb3ac6fd89eef1a2825b56ec4ddb6f32bcc
Dingyi <hoon_ding_yi@tech.gov.sg>
2020-10-15 16:12:45 +0800

    [DY]lint middleware.example

--- examples/middleware.example.ts
+++ examples/middleware.example.ts
@@ -1,5 +1,11 @@
 import express from 'express';
-import { enhanceReqWithTransactionAndTime, GLogger, LoggingMode, responseErrorLoggerFactory, responseSuccessLoggerFactory } from '../src';
+import {
+  enhanceReqWithTransactionAndTime,
+  GLogger,
+  LoggingMode,
+  responseErrorLoggerFactory,
+  responseSuccessLoggerFactory
+} from '../src';
 
 // EXAMPLE: INTIALIZING
 const logger = new GLogger({ loggingMode: LoggingMode.LOCAL });
@@ -11,21 +17,20 @@ const errorLogger = responseErrorLoggerFactory(logger, 'DWP', 'HRP', __filename,
 const server = express();
 
 server.use(enhanceReqWithTransactionAndTime);
-server.use(successLogger)
-server.get('/success',(req,res,next)=>{
-  res.send('success. see terminal for failure log examples')
+server.use(successLogger);
+server.get('/success', (req, res, next) => {
+  res.send('success. see terminal for failure log examples');
   next();
-})
-server.get('/fail',(req,res,next)=>{
-  res.send('fail. see terminal for failure log examples')
-  res.status(400)
-  next(new Error('failure error'))
-})
-server.use(errorLogger)
-const RANDOM_PORT = 57192
-server.listen(RANDOM_PORT,()=>{
-  logger.info(`Server started on port ${RANDOM_PORT}`)
-  logger.info(`run npm example-server and make get calls to /success and /fail`)
-})
+});
+server.get('/fail', (req, res, next) => {
+  res.send('fail. see terminal for failure log examples');
+  res.status(400);
+  next(new Error('failure error'));
+});
+server.use(errorLogger);
+const RANDOM_PORT = 57192;
+server.listen(RANDOM_PORT, () => {
+  logger.info(`Server started on port ${RANDOM_PORT}`);
+  logger.info(`run npm example-server and make get calls to /success and /fail`);
+});
 //#endregion
-
